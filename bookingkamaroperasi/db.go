04e8fa8432718b865bb9e792e9d6c0511077f3d4
Jason <jason.onggo@tempatkerja.com>
2022-12-29 10:15:46 +0700

    add thread safe db

--- bookingkamaroperasi/db.go
+++ bookingkamaroperasi/db.go
@@ -1,20 +1,40 @@
 package bookingkamaroperasi
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 type BookingKamarOperasi struct {
 	startDate time.Time
 	endDate   time.Time
 }
 
-var DB = map[time.Time][]BookingKamarOperasi{}
+type SafeDB struct {
+	mu sync.Mutex
+	v  map[time.Time][]BookingKamarOperasi
+}
+
+func (c *SafeDB) Append(key time.Time, bookingKamarOperasi BookingKamarOperasi) {
+	c.mu.Lock()
+	c.v[key] = append(c.v[key], bookingKamarOperasi)
+	c.mu.Unlock()
+}
+
+func (c *SafeDB) Value(key time.Time) []BookingKamarOperasi {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.v[key]
+}
+
+var c = SafeDB{v: make(map[time.Time][]BookingKamarOperasi)}
 
 func truncateDate(t time.Time) time.Time {
 	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
 }
 
-func IsThereNoScheduleIn2Hours(key time.Time, bookingDate time.Time) bool {
-	document := DB[key]
+func isThereNoScheduleIn2Hours(key time.Time, bookingDate time.Time) bool {
+	document := c.Value(key)
 
 	for _, tuple := range document {
 		diff := bookingDate.Sub(tuple.endDate)
@@ -29,16 +49,16 @@ func IsThereNoScheduleIn2Hours(key time.Time, bookingDate time.Time) bool {
 
 func TryAppend(bookingDate time.Time, duration time.Duration) bool {
 	yesterday := truncateDate(bookingDate.AddDate(0, 0, -1))
-	if !IsThereNoScheduleIn2Hours(yesterday, bookingDate) {
+	if !isThereNoScheduleIn2Hours(yesterday, bookingDate) {
 		return false
 	}
 
 	today := truncateDate(bookingDate)
-	if !IsThereNoScheduleIn2Hours(today, bookingDate) {
+	if !isThereNoScheduleIn2Hours(today, bookingDate) {
 		return false
 	}
 
-	DB[today] = append(DB[today],
+	c.Append(today,
 		BookingKamarOperasi{
 			startDate: bookingDate,
 			endDate:   bookingDate.Add(duration),
